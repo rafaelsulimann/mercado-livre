e12cab5e68c9a7afa855f8c159e9a2d90848f3e0
Scaffolder <scaffolder@backstage.io>
2024-01-16 21:35:06 -0300

    initial commit

---
+++ src/main/java/com/sulimann/mercadolivre/utils/TableName.java
@@ -0,0 +1,11 @@
+package com.sulimann.mercadolivre.utils;
+
+public final class TableName {
+
+    public static final String INIT = "tb_init";
+
+    private TableName() {
+        throw new AssertionError("Não é permitido instanciar esta classe.");
+    }
+    
+}
---
+++ src/main/java/com/sulimann/mercadolivre/validators/telefone/TelefoneValidator.java
@@ -0,0 +1,23 @@
+package com.sulimann.mercadolivre.validators.telefone;
+
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import main.java.com.sulimann.mercadolivre.utils.Regex;
+
+public class TelefoneValidator implements ConstraintValidator<Telefone, String>{
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (!StringUtils.hasText(value)) {
+            return false;
+        }
+        return value.matches(Regex.CELULAR);
+    }
+
+}
+
+
+
